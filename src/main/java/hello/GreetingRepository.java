3add75905522245ff35964a63e36fd91972bd6ee
journeymanroland <rolandggill@gmail.com>
2019-07-15 13:45:42 -0400

    deleted unnecessary package

--- src/main/java/hello/GreetingRepository.java
+++
@@ -1,9 +0,0 @@
-package hello;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GreetingRepository extends CrudRepository<Greeting, String> {
-
-}
